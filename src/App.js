85793095ae1c59b78986d1cf565f1ce8e623e0e2
h662 <godh662@gmail.com>
2020-01-21 13:10:56 +0900

    bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Header />
       <h1>헬로우 리액트</h1>
       <h2>안녕하세요</h2>
     </>
